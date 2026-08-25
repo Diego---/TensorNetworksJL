080c9c529cdecf5f809b7901002fc34cb6a28b5d
Diego Olvera Millán <omega_717@hotmail.com>
2026-08-25 12:31:06 +0200

    Update tensor.jl
    Add functions to extract the rank and the type of the underlying data Array's elements

--- src/tensor.jl
+++ src/tensor.jl
@@ -1,11 +1,45 @@
 """
-    Tensor{T,N}
+    Tensor{T, N}
 
 Dense rank-`N` tensor with elements of type `T`.
 
 # Fields
-- `data::Array{T,N}`: Tensor entries.
+- `data::Array{T, N}`: Tensor entries.
 """
-struct Tensor{T,N}
-    data::Array{T,N}
+struct Tensor{T, N}
+    data::Array{T, N}
+end
+
+"""
+    rank(tensor::Tensor) -> Int
+
+Return the rank (number of dimensions) of `tensor`.
+
+# Examples
+```jldoctest
+julia> t = Tensor(zeros(2, 3, 4));
+
+julia> rank(t)
+3
+```
+"""
+function rank(tensor::Tensor{T, N}) where {T, N}
+    return N
+end
+
+"""
+    data_type(tensor::Tensor) -> Type
+
+Return the type of the elements of `tensor`.
+
+# Examples
+```jldoctest
+julia> t = Tensor([2, 3, 4]);
+
+julia> data_type(t)
+Int64
+```
+"""
+function data_type(tensor::Tensor{T, N}) where {T, N}
+    return T
 end
