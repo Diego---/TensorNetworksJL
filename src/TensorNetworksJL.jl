b07630300b60c7447c0fd2b53e33e92de0d6b3d3
Diego Olvera Millán <omega_717@hotmail.com>
2026-08-31 15:59:26 +0200

    implement trotterization of a nearest neighbor hamiltonian time step

--- src/TensorNetworksJL.jl
+++ src/TensorNetworksJL.jl
@@ -4,7 +4,8 @@ include("tensor.jl")
 include("contractions.jl")
 include("mps.jl")
 include("mpo.jl")
+include("evolution.jl")
 
-export Tensor, contract, MPS, inner, svdcompress, entanglement_entropy, MPO, apply_MPO
+export Tensor, contract, MPS, inner, svdcompress, entanglement_entropy, MPO, apply_MPO, split_two_site_operator, evolution_operator, trotter_mpos
 
 end
